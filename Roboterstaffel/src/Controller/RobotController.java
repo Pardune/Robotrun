40c136ba5052543dd4bc83a251bc38f4b477bc00
Pardune <birger.lueers@uni-hannover.de>
2013-11-06 16:03:09 +0100

    controller extended

--- Roboterstaffel/src/Controller/RobotController.java
+++ Roboterstaffel/src/Controller/RobotController.java
@@ -3,7 +3,7 @@ package Controller;
 import java.util.*;
 import robot.Robot;
 
-public class RobotController implements Observer {
+public class RobotController implements EventListener {
 	private Robot[] robots;
 	/**
 	 * @param args
@@ -11,20 +11,19 @@ public class RobotController implements Observer {
 	public RobotController(){
 		robots[0] = new Robot();
 		robots[1] = new Robot();
+		robots[0].addEventListener(this);
+		robots[1].addEventListener(this);
 		
 		init();
 	}
 	private void init() {
 		// let robots search for podests
-		
+ 		
 		// somehow (?) evaluate which robot gets the Can initially
 		
 		// assign roles to Robots (fetch the can / get in position to receive the can)
 		// start communication between Robots
 		
-		
-	}
-	
-	
+	}	
 
 }
