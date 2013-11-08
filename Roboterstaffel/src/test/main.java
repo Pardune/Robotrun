244fbcdd80c7db43d193861918c9d76df85442a1
user <user@user-W3x0ET>
2013-11-08 15:16:52 +0100

    add distance array

--- Roboterstaffel/src/test/main.java
+++ Roboterstaffel/src/test/main.java
@@ -1,7 +1,15 @@
 package test;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+
 import lejos.nxt.Motor;
+import lejos.nxt.SensorPort;
+import lejos.nxt.UltrasonicSensor;
+import lejos.nxt.comm.NXTConnection;
+import lejos.nxt.comm.USB;
 import lejos.robotics.navigation.DifferentialPilot;
+import lejos.util.Delay;
 
 public class main {
 
@@ -9,8 +17,23 @@ public class main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		DifferentialPilot pilot = new DifferentialPilot(43.2f, 4.4f, Motor.A, Motor.C, false);
-		pilot.travel(100);
+		
+		DifferentialPilot pilot = new DifferentialPilot(43.2f, 161f, Motor.A, Motor.B, false);
+		//pilot.travel(1000);
+		
+		pilot.setAcceleration(30);
+		//for(int x = 0; x <10; x++) {
+		pilot.rotate(360);
+		UltrasonicSensor us = new UltrasonicSensor(SensorPort.S4);
+		int[] rotationArray = new int[36];
+		for(int rotation = 0; rotation < 36; rotation = rotation++ ) {
+			while (rotation*10 > pilot.getAngleIncrement()) {
+				Delay.msDelay(10);
+			}
+			rotationArray[rotation]=us.getDistance();
+		}
+		
+		
 	}
 
 }
