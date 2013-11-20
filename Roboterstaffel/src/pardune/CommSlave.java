b59ef79a2e67bb1b408566d54db593bf4bbfc51b
schust <schuler.st@web.de>
2013-11-20 10:52:32 +0100

    javadocs added/some unused methods deleted

--- Roboterstaffel/src/pardune/CommSlave.java
+++ Roboterstaffel/src/pardune/CommSlave.java
@@ -1,44 +1,46 @@
 package pardune;
-/**
- * this program receives data from a master
- * 
- */
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-
-import javax.bluetooth.RemoteDevice;
-
 import lejos.nxt.comm.BTConnection;
 import lejos.nxt.comm.Bluetooth;
 
+/**
+ * this program receives data from a master
+ * 
+ *
+ */
 public class CommSlave {
-	 // fields for constuctor CommMaster()
+	
 	DataInputStream dis;
 	DataOutputStream dos;
 	BTConnection btc;
-	RemoteDevice btrd;
+	//RemoteDevice btrd;
 	String remoteName = "NXT";
 
 	// fields for sendData()
 	int data;
 
+	/**
+	 * Waits for an incoming connection from master robot.
+	 */
 	public CommSlave() {
 		try {
-			// setup for pcToSource()
 			btc = Bluetooth.waitForConnection();
 			dis = btc.openDataInputStream();
 			dos = btc.openDataOutputStream();
 
-			// setup for nxtToNxt()
-			btrd = Bluetooth.getKnownDevice(remoteName);
-			// setup end
+			//btrd = Bluetooth.getKnownDevice(remoteName);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
+	/**
+	 * Blocks till other robot sends a 1 (is ready).
+	 */
 	public void waitForAnswer() {
 		try {
 			while (dis.readInt() != 1) {
@@ -50,6 +52,9 @@ public class CommSlave {
 		}
 	}
 	
+	/**
+	 * Sends a 1 to the other robot, to tell him to go on.
+	 */
 	public void sendReady() {
 		try {
 			dos.writeInt(1);
@@ -60,11 +65,22 @@ public class CommSlave {
 		}
 	}
 
-	public void end() throws Exception{
-		// obligatory
-		dis.close();
-		dos.close();
-		Thread.sleep(100);			// better do this
+	/**
+	 * Closes input and output stream of the connection.
+	 */
+	public void end() {
+		try {	
+			// obligatory
+			dis.close();
+			dos.close();
+			Thread.sleep(100);			//wait for streams being closed
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
 
