f01ad3bbfc9cd7cbff153c3603082c8bff9f9fd7
Birger Lüers <blueers@gmail.com>
2013-11-21 03:55:46 +0100

    theoretischer kram, proposals zur podesterkennung

--- Roboterstaffel/src/pardune/ObjectsPardune.java
+++ Roboterstaffel/src/pardune/ObjectsPardune.java
@@ -7,10 +7,12 @@ import lejos.util.Delay;
 
 public class ObjectsPardune {
 
-	int prevVal, curVal;
+	int prevVal, curVal, nexVal;
 	boolean foundPodest;
 	DifferentialPilot pilot;
 	UltrasonicSensor ultrasonicSensor;
+	private int numValidMeasurements;
+
 	private int measured;
 	private int[] measurements;
 	private int[] differenceBetweenVals;
@@ -27,14 +29,132 @@ public class ObjectsPardune {
 		foundPodest = false;
 
 		// run the actual program
-		//findPodest();
 		alg();
 	}
+	
+	/**
+	 *  es gibt 4 arten, das podest zu finden.
+	 *  
+	 *  kurvendiskussion:
+	 *  suchen von maxima (ecken).
+	 *  zwischen diesen gibt es lokale minima. diese stellen podeste,
+	 *  roboter, oder eine mauer dar.
+	 *  
+	 *  
+	 *  einteilen in dreiecke
+	 *  der roboter teilt das Feld mit seiner Position in vier Dreiecke
+	 *  ein, deren hypotenuse immer 2m beträgt.
+	 *  die Eckpunkte der Dreiecke sind Ecken des Feldes und damit absolute Maxima.
+	 *  ein Podest hat das geringste minimum zwischen den beiden absoluten Maxima.
+	 *  
+	 *  
+	 *  einteilen in rechtecke
+	 *  lokale minima stellen wände dar, somit kann das feld in vierecke eingeteilt werden.
+	 *  In diesen gibt es ein absolutes maxima (ecke) und evtl ein absolutes minimum(podest)
+	 *  
+	 *  
+	 *  fahren in die mitte
+	 *  der roboter fährt zur größten distanz, stoppt bei der Mittellinie (diese wird er
+	 *  tangieren), richtet sich an der linie, in der mitte aus, scannt seine umgebung um
+	 *  180° und sucht ein von den bekannten messwerten abweichenden wert. dieser stellt
+	 *  das podest dar. 
+	 *  
+	 *  
+	 *  		hilfe ein hai
+	 *                   \o/		/(
+	 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	        o		o           O 		8
+	      °         °°       °°       °
+	 
+	 *  
+	 */
+	
 
 	private void alg() {
-		// TODO Auto-generated method stub
+		// find podest roughly
+		measurements = new int[72];
+		findPodestRoughly();
+
+		// validate
+		// search for maxima in measurements[]
+		maximaInMeasurements();
+
+		// get closer, to safe measurement distance
+		// increase precision
+
+		// get can
+
+		// dive to delivery point
+
+
+	}
+
+
+	private void findPodestRoughly() {
+		int tempMeasured;
+		for (int i=0; i < 72; i++) {
+			measured = 0;
+			numValidMeasurements = 0;
+			for(int j = 0; j<5; j++) {
+				tempMeasured = ultrasonicSensor.getDistance();
+				if (tempMeasured != 255) {
+					System.out.println("usable value, " + j);
+					measured += tempMeasured;
+					numValidMeasurements++;
+				}
+			}
+			// check for faulty data
+			if(numValidMeasurements == 0) {
+				System.out.println("no usable data found, set 255, " +i);
+				measurements[i] = 255;
+			} else {
+				measurements[i] = (int) (measured/numValidMeasurements);
+			}
+			pilot.rotate(5);				// 5 degrees right
+			while(pilot.isMoving()) Thread.yield();
+		}
+		System.out.println("measurements-Array filled");
+	}
+
+
+	private void maximaInMeasurements() {
+		int[] lokalesMaximum = new int[10];
+		int lokalesMaximumIndex = 0;
+
+		// es gibt bei jeder drehung maximal 4 (de facto wahrsch 3) maxima.
+		// hinzu kommen evtl. lokale für
+		// 1. das Podest
+		// 2. das andere Podest
+		// 3. den anderen roboter
+
+		// Maxima in array speichern
+		for (int i=1; i < 71; i++) {
+			prevVal = measurements[i-1];
+			curVal = measurements[i];
+			nexVal = measurements[i+1];
+
+			// maximum: werte steigen an, bis ein kleinerer kommt
+			if (curVal > prevVal && curVal > nexVal){
+				// an Stelle i ist ein Maximum
+				lokalesMaximum[lokalesMaximumIndex] = i;
+				lokalesMaximumIndex++;
+			}
+		}
+		// alle maxima in ein array gespeichert
+		System.out.println("lokale Maxima gef.:" + lokalesMaximumIndex);
+	}
+	
+	private void (){
+		
 		
 	}
+
+	/***
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
 
 	private void findPodest() {
 		measurements = new int[72];
@@ -43,7 +163,7 @@ public class ObjectsPardune {
 		// if: corner(pos to neg): continue
 		// else: retreive dose (pos to neg to pos)
 
-		// fill measurments and diff-array via loop
+		// fill measurements and diff-array via loop
 		int numValidMeasurements = 0;
 		for (i=0; i < 72; i++) {
 			for(int j = 0; j<5; j++) {
@@ -106,13 +226,13 @@ public class ObjectsPardune {
 			System.out.println("Index ist: " + i); 
 		}
 	}
-	
+
 	public void findPodestPar(int vals){
 		measurements = new int[vals];
 		pilot.rotate((vals-1)/2);
-		
+
 	}
-	
+
 	public int getPeak(){
 		return measurements[i];
 	}
